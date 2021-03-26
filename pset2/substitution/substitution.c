680bf78c38ccbe9d9f36b1d41b7269d9214f182a
skyfall01 <33621393+skyfall01@users.noreply.github.com>
2021-03-26 23:02:30 +0530

    Update substitution.c

--- pset2/substitution/substitution.c
+++ pset2/substitution/substitution.c
@@ -3,78 +3,92 @@
 #include <string.h>
 #include <ctype.h>
 
-int isValid(string);
+int isValidKey(string);
+
 int main(int argc, string argv[])
 {
-    if (argc == 2)
+    // No definite key is given or too many/less arguments
+    if (argc != 2)
     {
-        if (strlen(argv[1]) == 26)
-        {
-            if (isValid(argv[1]))
-            {
-                string plaintext = get_string("plaintext: ");    //User input
-                int length = strlen(plaintext);
-                char character;
-                printf("ciphertext: ");
-                for (int i = 0; i < length; i++)
-                {
-                    character = tolower(plaintext[i]);
-                    if (character >= 'a' && character <= 'z')
-                    {
-                        character = tolower(argv[1][(character - 97)]);  //Get the index of plaintext in key
-                        if (isupper(plaintext[i]))
-                        {
-                            character = toupper(character);      //If it's a upper case letter, convert accordlingly
-                        }
-                    }
-                    printf("%c", character);          //print the letter
-                }
-                printf("\n");
-                return 0;
-            }
-            else
-            {
-                printf("Contains duplicate/non alphabets\n");
-                return 1;
-            }
-        }
-        else
-        {
-            printf("Key must contain 26 characters.\n");           //Key length is short
-            return 1;
-        }
-    }
-    else
-    {
-        printf("Usage: %s key\n", argv[0]);         //No definite key is given or too many/less arguments
+        printf("Usage: %s key\n", argv[0]);
         return 1;
     }
+
+    //Key(argv[1]) length is short
+    if (strlen(argv[1]) != 26)
+    {
+        printf("Key must contain 26 characters.\n");
+        return 1;
+    }
+
+    // If duplicate characters/ non-alphabets in the key
+    if (isValidKey(argv[1]) == 0)
+    {
+        printf("Contains duplicate/non alphabets\n");
+        return 1;
+    }
+
+    // Get input from user
+    string plaintext = get_string("plaintext: ");
+    int length = strlen(plaintext);
+    char character;
+
+    // Print the cipher text using key for plain-text
+    printf("ciphertext: ");
+    for (int i = 0; i < length; i++)
+    {
+        character = plaintext[i];
+        if (isalpha(character))
+        {
+            // Convert letter to lowercase if applicable
+            // Then subtract its ascii value with 97 to get an absolute index in range(0 to 25) - [26 alphabets]
+            int index = tolower(character) - 97;
+
+            // Upper cipher character for uppercase letter in plain-text
+            if (isupper(character))
+            {
+                character = toupper(argv[1][index]);
+            }
+
+            // Lower cipher character for lowercase letter in plain-text
+            else
+            {
+                character = tolower(argv[1][index]);
+            }
+        }
+        printf("%c", character);
+    }
+    printf("\n");
+    return 0;
 }
 
-
-int isValid(string key)
+int isValidKey(string key)
 {
     int i;
-    int key1[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int letterCount[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // Length: 26
     for (i = 0; i < 26; i++)
     {
-        if (isalpha(key[i]))                       //Check whether character in key is alphabet
+        //Check whether character in key is alphabet
+        if (isalpha(key[i]))
         {
             int index = tolower(key[i]) - 97;
-            if (key1[index] == 0)              //Test frequency is singular
+
+            // Check if we are seeing the letter for the very first time
+            if (letterCount[index] == 0)
             {
-                ++key1[index];
+                ++letterCount[index];
                 continue;
             }
         }
         break;
     }
-    if (i == 26)            //Key is valid
+
+    //Key is valid
+    if (i == 26)
     {
         return 1;
     }
-    else                   //Key is Invalid
-    {
-        return 0;
-    }
+
+    //Key is Invalid
+    return 0;
 }
