2d4b14d7444e61fa6c601e6aae9d63f472a327ee
skyfall01 <33621393+skyfall01@users.noreply.github.com>
2021-03-26 22:39:52 +0530

    Update readability.c

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -6,48 +6,65 @@
 
 int main(void)
 {
-    string text = get_string("Text: "); //Get the input from user
-    int letters = 0, sentences = 0, words = 0, i, length = strlen(text);
-    float L, S;
-    for (i = 0; i < length && words <= 100; i++)                                      //In this loop we count sentences, words, letters
+    string text = get_string("Text: ");
+    float letters = 0, sentences = 0, words = 0;
+    int i, length = strlen(text);
+
+    // Iterate through every character to count letters, words, sentences
+    for (i = 0; i < length && words <= 100; i++)
     {
-        if (tolower(text[i]) >= 'a' && tolower(text[i]) <= 'z')          //The character is a letter
+
+        // The character is a letter
+        if (tolower(text[i]) >= 'a' && tolower(text[i]) <= 'z')
         {
             ++letters;
         }
-        else if (text[i] == ' ')        //The character is a white space
+
+        // The character is a white space
+        else if (text[i] == ' ')
         {
             ++words;
         }
-        else if (text[i] == '.' || text[i] == '!' || text[i] == '?')             //The character is a question, exclamation mark or period
+
+        // The character is a question, exclamation mark or period
+        else if (text[i] == '.' || text[i] == '!' || text[i] == '?')
         {
             ++sentences;
         }
+
+        // Don't bother otherwise
         else
         {
             continue;
         }
     }
+
+    // If we have reached the end of text, then add the last word to wordCount since it doesn't end with '.', '!', '?'
     if (i == length)
     {
-        ++words; //Count last word if we are finished and at the end of sentence
+        ++words;
     }
-    L = letters;
-    S = sentences;
+
+    // If words < 100, count the avg number of letters and sentences per 100 words
     if (words < 100)
     {
-        L = (L * 100) / words; //If words < 100, count the avg number of letters and sentences per 100 words
-        S = (S * 100) / words;
+        letters = (letters * 100.0) / words;
+        sentences = (sentences * 100.0) / words;
     }
-    int index = round((0.0588 * L) - (0.296 * S) - 15.8); //Coleman-Liau index for grade calculation
+
+    // Coleman-Liau index for grade calculation
+    int index = round((0.0588 * letters) - (0.296 * sentences) - 15.8);
+
     if (index < 1)
     {
         printf("Before Grade 1\n");
     }
+
     else if (index > 16)
     {
         printf("Grade 16+\n");
     }
+
     else
     {
         printf("Grade %i\n", index);
