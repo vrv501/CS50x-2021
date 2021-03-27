99367bbb8c5b8bcff095b76b971a7e35f1f4d6a6
skyfall01 <33621393+skyfall01@users.noreply.github.com>
2021-03-27 23:41:50 +0530

    Update dictionary.c

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -1,47 +1,51 @@
 // Implements a dictionary's functionality
-
 #include <stdbool.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
-
 #include "dictionary.h"
 
 // Represents a node in a trie
-typedef struct node
+typedef struct trie
 {
-    bool end;
-    struct node *next[27];
-}
-node;
+    bool endOfword;
+    struct trie *next[27];
+} trie;
+
+void freemem(trie *);
 
 // Number of buckets in trie
 const unsigned int N = 27;
+
 // Trie root
-node *root;
+trie *root;
 unsigned int word_counter = 0;
 
-//Returns a new node
-node *get_node(void)
+// Returns a new node
+trie *get_node(void)
 {
-    node *n = malloc(sizeof(node));
-    n->end = false;
-    if (n != NULL)
+    trie *node = malloc(sizeof(trie));
+    node->endOfword = false;
+
+    if (node != NULL)
     {
         for (int i = 0; i < N; i++)
         {
-            n->next[i] = NULL;
+            node->next[i] = NULL;
         }
     }
-    return n;
+    return node;
 }
 
-int get_index(char c)
+int get_index(char character)
 {
-    if (isalpha(c))
+    // If character is alphabet, return alphabet position [0 to 25]
+    if (isalpha(character))
     {
-        return tolower(c) - 97;
+        return tolower(character) - 97;
     }
+
+    // If not just return 26
     else
     {
         return 26;
@@ -53,22 +57,28 @@ bool check(const char *word)
 {
     // TODO
     int index;
-    if (root != NULL)
+    if (root == NULL)
     {
-        node *temp = root;
-        for (int i = 0; word[i] != '\0'; i++)
+        return false;
+    }
+
+    trie *temp = root;
+    for (int i = 0; word[i] != '\0'; i++)
+    {
+        index = get_index(word[i]);
+
+        // If the next node is NULL, the word isn't in dictionary
+        if (temp->next[index] == NULL)
         {
-            index = get_index(word[i]);
-            if (temp->next[index] == NULL)
-            {
-                return false;
-            }
-            temp = temp->next[index];
+            return false;
         }
-        if (temp->end)
-        {
-            return true;
-        }
+        temp = temp->next[index];
+    }
+
+    // If end of node is true, then we have found the word in dictionary
+    if (temp->endOfword)
+    {
+        return true;
     }
     return false;
 }
@@ -78,40 +88,49 @@ bool check(const char *word)
 bool load(const char *dictionary)
 {
     FILE *file = fopen(dictionary, "r");
-    if (file != NULL)
+
+    if (file == NULL)
     {
-        char word[LENGTH + 1];
-        int index;
-        root = get_node();
-        if (root == NULL)
-        {
-            return false;
-        }
-        node *temp, *nextnode;
-        while (fscanf(file, "%s", word) != EOF)
-        {
-            temp = root;
-            for (int i = 0; word[i] != '\0'; i++)
-            {
-                index = get_index(word[i]);
-                if (temp->next[index] == NULL)
-                {
-                    nextnode = get_node();
-                    if (nextnode == NULL)
-                    {
-                        return false;
-                    }
-                    temp->next[index] = nextnode;
-                }
-                temp = temp->next[index];
-            }
-            temp->end = true;
-            word_counter++;
-        }
-        fclose(file);
-        return true;
+        return false;
     }
-    return false;
+
+    root = get_node();
+    if (root == NULL)
+    {
+        return false;
+    }
+
+    char word[LENGTH + 1];
+    int index;
+    trie *temp, *nextnode;
+    while (fscanf(file, "%s", word) != EOF)
+    {
+        temp = root;
+        for (int i = 0; word[i] != '\0'; i++)
+        {
+            index = get_index(word[i]);
+
+            // If the next node is Null then attach a new node
+            if (temp->next[index] == NULL)
+            {
+                nextnode = get_node();
+                if (nextnode == NULL)
+                {
+                    return false;
+                }
+                temp->next[index] = nextnode;
+            }
+
+            // If next node isn't NULL, then we move down the trie
+            temp = temp->next[index];
+        }
+
+        temp->endOfword = true;
+        word_counter++;
+    }
+
+    fclose(file);
+    return true;
 }
 
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
@@ -119,19 +138,6 @@ unsigned int size(void)
 {
     // TODO
     return word_counter;
-}
-
-//Recursively remove trie nodes
-void freemem(node *trie)
-{
-    if (trie)
-    {
-        for (int i = 0; i < N; i++)
-        {
-            freemem(trie->next[i]);
-        }
-        free(trie);
-    }
 }
 
 // Unloads dictionary from memory, returning true if successful else false
@@ -148,4 +154,17 @@ bool unload(void)
     }
 
     return false;
+}
+
+//Recursively remove trie nodes
+void freemem(trie *node)
+{
+    if (node)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            freemem(node->next[i]);
+        }
+        free(node);
+    }
 }
