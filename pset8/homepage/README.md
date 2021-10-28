9390e719b1c7d242c45873e7a9abbe1fdfc0d8fd
skyfall01 <33621393+skyfall01@users.noreply.github.com>
2021-10-28 21:09:18 +0530

    Delete pset8/homepage directory

--- pset8/homepage/README.md
+++
@@ -1,2 +0,0 @@
-# Homepage
-Prof Malan's personal website in my own style. [Watch demo](https://vimeo.com/526587367)
--- pset8/homepage/navBarDynamic.js
+++
@@ -1,7 +0,0 @@
-function myFunction() {
-    let y = document.getElementsByTagName("ul");
-    if (y[0].className == "nav_menu")
-      y[0].className = "nav_menu responsive";
-    else
-      y[0].className = "nav_menu";
-  }
